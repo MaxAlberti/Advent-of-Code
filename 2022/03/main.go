c2fa1444ae439e9532021925007c2b77b6fa007a
MaxAlberti <maxalberti@gmx.de>
2022-12-22 14:21:43 +0100

    day3 progress - create_empty_rucksack

--- 2022/03/main.go
+++ 2022/03/main.go
@@ -8,93 +8,58 @@ import (
 	"os"
 )
 
-type Sign struct {
-	Value     int
-	Name      string
-	Beats     string
-	BeatsChar string
+type Item struct {
+	Priority int
+	Name     string
+	Count    int
 }
 
-var sign_map = make(map[string]Sign)
+type Rucksack struct {
+	Comp1 map[string]Item
+	Comp2 map[string]Item
+}
 
 // Main function
 func main() {
 	fmt.Println("Starting")
-	generate_signs()
-	lines, err := get_file_lines("input.txt")
+	_, err := get_file_lines("test.txt")
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
+		os.Exit((1))
 	}
-	result1, result2 := calculate_outcome(lines, 0, 3, 6)
-	fmt.Printf("P1: Total player score: %d\nP2: Total player score: %d", result1, result2)
+	create_empty_rucksack()
 }
 
-func calculate_outcome(lines []string, lose_score int, draw_score int, win_score int) (int, int) {
-	p1_total_score := 0
-	p2_total_score := 0
-	for _, line := range lines {
-		elf_char := string(line[0:1])
-		player_char := string(line[2:3])
-		// Convert to signs
-		elf_sign := sign_map[elf_char]
-		player_sign := sign_map[player_char]
-		// P1 - Calculate round result
-		round_res := player_sign.Value
-		if elf_sign.Name == player_sign.Name {
-			round_res += draw_score
-		} else if player_sign.Beats == elf_sign.Name {
-			round_res += win_score
-		} else {
-			round_res += lose_score
-		}
-		p1_total_score += round_res
-		// P2 - Calculate round result
-		round_res = 0
-		if player_char == "X" {
-			// Neet to loose
-			player_sign = sign_map[elf_sign.BeatsChar]
-			round_res += player_sign.Value
-			round_res += lose_score
-		} else if player_char == "Y" {
-			// Need draw
-			player_sign = sign_map[elf_char]
-			round_res += player_sign.Value
-			round_res += draw_score
-		} else {
-			// Need to win
-			player_sign = sign_map[sign_map[elf_sign.BeatsChar].BeatsChar]
-			round_res += player_sign.Value
-			round_res += win_score
-		}
-		p2_total_score += round_res
+func create_empty_rucksack() Rucksack {
+	var r Rucksack
+	// create empty compartments
+	r.Comp1 = make(map[string]Item)
+	r.Comp2 = make(map[string]Item)
+	i := 97
+	item_prio := 1
+	for i <= 122 {
+		var item Item
+		item.Count = 0
+		item.Name = string(byte(i))
+		item.Priority = item_prio
+		r.Comp1[string(byte(i))] = item
+		r.Comp2[string(byte(i))] = item
+		item_prio += 1
+		i += 1
+	}
+	i = 65
+	for i <= 90 {
+		var item Item
+		item.Count = 0
+		item.Name = string(byte(i))
+		item.Priority = item_prio
+		r.Comp1[string(byte(i))] = item
+		r.Comp2[string(byte(i))] = item
+		item_prio += 1
+		i += 1
 	}
 
-	return p1_total_score, p2_total_score
-}
-
-func generate_signs() {
-	var rock Sign
-	rock.Value = 1
-	rock.Beats = "Scissors"
-	rock.Name = "Rock"
-	rock.BeatsChar = "C"
-	var paper Sign
-	paper.Value = 2
-	paper.Beats = "Rock"
-	paper.Name = "Paper"
-	paper.BeatsChar = "A"
-	var scissors Sign
-	scissors.Value = 3
-	scissors.Beats = "Paper"
-	scissors.Name = "Scissors"
-	scissors.BeatsChar = "B"
-	sign_map["A"] = rock
-	sign_map["B"] = paper
-	sign_map["C"] = scissors
-	sign_map["X"] = rock
-	sign_map["Y"] = paper
-	sign_map["Z"] = scissors
+	return r
 }
 
 func get_file_lines(filepath string) ([]string, error) {
